76861badbe336796e0daf28f31b39458263c52b0
Aleksey Baydyk <lehalevan@gmail.com>
2019-11-19 21:28:19 +0200

    Updates

---
+++ precache-manifest.3ebe455d81a7ff2c57c9f43f91009cbf.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba09c491435d717d60484aba48ff23cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd35a8b0b73b82aa8c0b",
+    "url": "/static/css/2.0c2e4d29.chunk.css"
+  },
+  {
+    "revision": "0114f03cd861d1c1eea6",
+    "url": "/static/css/main.2bc65e97.chunk.css"
+  },
+  {
+    "revision": "dd35a8b0b73b82aa8c0b",
+    "url": "/static/js/2.d81d867d.chunk.js"
+  },
+  {
+    "revision": "0114f03cd861d1c1eea6",
+    "url": "/static/js/main.9e1e264b.chunk.js"
+  },
+  {
+    "revision": "df8891552ec74e4b59f0",
+    "url": "/static/js/runtime-main.1ec05612.js"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/static/media/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/static/media/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/static/media/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "da4ea5cdfca6b3baab285741f5ccb59f",
+    "url": "/static/media/MaterialIcons-Regular.da4ea5cd.ttf"
+  }
+]);
